1bd5ac01cd02a259df2d6b2ae64dd9182513f21b
kor0 <ryusama6996@gmail.com>
2023-08-08 14:40:13 +0530

    fix(src):added cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import dotenv from 'dotenv';
 import express, { Application } from 'express';
 import TestRoute from './routes/test';
+import cors from 'cors';
 import dbConnect from './db_connection';
 class Server {
     private app: Application;
@@ -9,6 +10,7 @@ class Server {
         dotenv.config();
         const PORT: number = Number(process.env["PORT"]) ?? 8000;
         this.app = express();
+        this.app.use(cors());
         this.app.use(express.json());
         this.initializeRoutes();
         const db = new dbConnect();
